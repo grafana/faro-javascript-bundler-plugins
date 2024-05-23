1ec70d7ae4c6dc8b89e1937675a4001e8c1e3d82
Elliot Kirk <elliot.kirk@grafana.com>
2024-05-23 16:13:24 -0700

    webpack testing done

--- packages/faro-webpack/src/index.test.js
+++ packages/faro-webpack/src/index.test.js
@@ -1,9 +1,11 @@
 import test from 'ava';
 import webpack from 'webpack';
 import config from './test/webpack.config.js';
+import fs from 'fs';
 
 test('webpack', async t => {
   const stats = await webpack(config);
-  t.snapshot(stats);
-  t.pass();
+
+  const content = fs.readFileSync(`${stats.outputPath}/../src/test/dist/bundle.cjs`, 'utf-8');
+  t.truthy(content.startsWith(`!function(){try{("undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{})["__faroBundleId_webpack-test-app"]`));
 });
