93da97bfc77258387264eef011073360126789e1
Jure Rotar <jrotar0@gmail.com>
2026-05-19 21:17:26 +0200

    chore: bumped yarn to v4, removed redundant "packageManager" fields, added minor simplifications to rollup configs (#545)
    Co-authored-by: Elliot Kirk <elliot@elliotkirk.com>

--- packages/faro-esbuild/rollup.config.mjs
+++ packages/faro-esbuild/rollup.config.mjs
@@ -3,15 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
-import fs from 'node:fs';
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
-const configPath = join(
-  dirname(fileURLToPath(import.meta.url)),
-  './package.json'
-);
-const packageJson = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+import packageJson from './package.json' with { type: 'json' };
 
 const extensions = [".ts"];
 
